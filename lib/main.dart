7605653118803e1bf938cc0d1fc2350e837263e9
yiyayamaya <892223116@qq.com>
2019-03-01 19:37:25 +0800

    keep

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,28 @@
 import 'package:flutter/material.dart';
+import 'global_config.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
-
-  var outvalue=1;
-  FavoriteWidget myf=new FavoriteWidget();
-
   
+ 
+
   @override
   Widget build(BuildContext context) {
-    Widget firstw = Container(
-      child: myf,
-    );
+   
 
     return MaterialApp(
       title: '文字冒险',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('这是一个文字冒险'),
-          
-        ),
-        body: ListView(
-          children: [
-
-
-              IconButton(
-          icon: (Icon(Icons.star)),
-          color: Colors.red[500],
-          onPressed: changevalue,
-        ),
-
-             new Text(
-              outvalue.toString()),
-            firstw,
-          ],
-        ),
-      ),
+      home:  new FavoriteWidget(),
     );
   }
+
   
 
-void changevalue(){
-print(myf.hashCode);
+  
+}
 
-print(myf.createState().hashCode);
-}
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column();
-  }
-}
+
 
 // #docregion FavoriteWidget
 class FavoriteWidget extends StatefulWidget {
@@ -58,24 +30,40 @@ class FavoriteWidget extends StatefulWidget {
   _FavoriteWidgetState createState() => _FavoriteWidgetState();
 }
 
-
-
 class _FavoriteWidgetState extends State<FavoriteWidget> {
-
-  
   var questionlist = ["第一个问题", "第二个问题", "第三个问题"];
 
-  String thetext = '';
 
-  int indexofques = 0;
+  
+  
+int indexofques = 0;
+  static List<List<String>> listofanswer=[['1A','1B','1C','1D'],['2A','2B','2C','2D'],['3A','3B','3C','3D'],['4A','4B','4C','4D']];
+
+
+ 
+ var thetext='';
+ 
+  var choiceA='';
+    var choiceB='';
+    var choiceC='';
+    var choiceD='';
+ 
+  
+  
+  
+ 
+  int  indexofword=0;
+  var correctchoice='';
+
+
   void pressstar() {
     setState(() {
+       
       indexofques++;
       if (indexofques >= questionlist.length) {
         indexofques = indexofques - questionlist.length;
       }
       thetext = questionlist[indexofques];
-     
     });
   }
 
@@ -86,28 +74,90 @@ class _FavoriteWidgetState extends State<FavoriteWidget> {
         indexofques = indexofques - questionlist.length;
       }
       thetext = questionlist[indexofques];
-
     });
   }
- 
+
+   makechoice(var mychoice){
+
+    setState(() {
+      if(mychoice==correctchoice)
+    {
+      indexofword++;
+    }
+    });
+
+    
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    //var choiceA=(listofanswer[indexofques])[0];
+   //var choiceB=(listofanswer[indexofques])[1];
+    //var choiceC=(listofanswer[indexofques])[2];
+    // var choiceD=(listofanswer[indexofques])[3];
+
+  
     return Row(
-      mainAxisSize: MainAxisSize.min,
+     
       children: [
-        IconButton(
-          icon: (Icon(Icons.star)),
+
+        
+
+        /*
+        new IconButton(
+          icon: new Icon(Icons.star),
           color: Colors.red[500],
           onPressed: pressstar,
         ),
-        IconButton(
-          icon: (Icon(Icons.ac_unit)),
+        new IconButton(
+          icon: new Icon(Icons.ac_unit),
           color: Colors.blue[500],
           onPressed: presssnow,
-        ),
-        Text(thetext),
+        ),*/
+      
+        new RaisedButton(child:new Text(choiceA),
+          onPressed:makechoice('B'),),
+        
+        new RaisedButton(child:new Text(choiceB),
+          onPressed:makechoice('B'),),
+        new RaisedButton(child:new Text(choiceC),
+         onPressed:makechoice('C'),),
+        new RaisedButton(child:new Text(choiceD),
+         onPressed:makechoice('D'),),
+
+
+        /*
+         new Text(
+              '学习Text',
+              textAlign: TextAlign.center, //文本对齐方式  居中
+              textDirection: TextDirection.ltr, //文本方向
+              softWrap: false, //是否自动换行 false文字不考虑容器大小  单行显示   超出；屏幕部分将默认截断处理
+              overflow: TextOverflow
+                  .ellipsis, //文字超出屏幕之后的处理方式  TextOverflow.clip剪裁   TextOverflow.fade 渐隐  TextOverflow.ellipsis省略号
+              textScaleFactor: 2.0, //字体显示的赔率
+              maxLines: 10, //最大行数
+              style: new TextStyle(
+                decorationColor: const Color(0xffffffff), //线的颜色
+                decoration: TextDecoration
+                    .none, //none无文字装饰   lineThrough删除线   overline文字上面显示线    underline文字下面显示线
+                decorationStyle: TextDecorationStyle
+                    .solid, //文字装饰的风格  dashed,dotted虚线(简短间隔大小区分)  double三条线  solid两条线
+                wordSpacing: 0.0, //单词间隙(负值可以让单词更紧凑)
+                letterSpacing: 0.0, //字母间隙(负值可以让字母更紧凑)
+                fontStyle: FontStyle.italic, //文字样式，斜体和正常
+                fontSize: 20.0, //字体大小
+                fontWeight: FontWeight.w900, //字体粗细  粗体和正常
+                color: const Color(0xffffffff), //文字颜色
+              ),
+            ),
+         */
+       
+
+      
+
+
       ],
     );
   }
-
 }
